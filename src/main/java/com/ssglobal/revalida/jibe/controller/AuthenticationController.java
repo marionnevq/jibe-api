faaf8af2f09ae45b960566db496a04b5a7524fd3
marionnevq <quintanamarionne@gmail.com>
2023-02-20 16:59:57 +0800

    added validation for register and authenticate

--- src/main/java/com/ssglobal/revalida/jibe/controller/AuthenticationController.java
+++ src/main/java/com/ssglobal/revalida/jibe/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import com.ssglobal.revalida.jibe.dto.RegisterRequest;
 import com.ssglobal.revalida.jibe.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,13 +20,28 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authenticationService.register(request));
+    public ResponseEntity<Object> register(@RequestBody RegisterRequest request) {
+        String error = "";
+        try {
+            return ResponseEntity.ok(authenticationService.register(request));
+        } catch (Exception e) {
+            error = e.getMessage();
+        }
+        return ResponseEntity.status(409).body(error);
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
-        return ResponseEntity.ok(authenticationService.authenticate(request));
+    public ResponseEntity<Object> authenticate(@RequestBody AuthenticationRequest request) {
+        String error = "";
+        try {
+            return ResponseEntity.ok(authenticationService.authenticate(request));
+        } catch (Exception e) {
+            if (e instanceof BadCredentialsException) {
+                error = "Invalid Credentials";
+            }
+        }
+
+        return ResponseEntity.status(401).body(error);
 
     }
 
