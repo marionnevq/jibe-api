ff3d8179f93736eb0dc686febc1078c6a0595b15
marionnevq <quintanamarionne@gmail.com>
2023-02-25 12:38:25 +0800

    added get following posts api

--- src/main/java/com/ssglobal/revalida/jibe/repository/PostRepository.java
+++ src/main/java/com/ssglobal/revalida/jibe/repository/PostRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssglobal.revalida.jibe.model.Post;
 
+import java.util.Collection;
 import java.util.List;
 
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findByUser_IdInOrderByDatePostedDesc(Collection<Integer> ids);
     List<Post> findByUser_UsernameOrderByDatePostedDesc(String username);
     List<Post> findByPostIDNotNullOrderByDatePostedDesc();
 
