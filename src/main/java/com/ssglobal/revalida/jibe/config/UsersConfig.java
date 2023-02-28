8fbc053ff6842cf98fed26cb8557fe048f0f9624
JayannisDname <87741012+JayannisDname@users.noreply.github.com>
2023-02-28 11:38:07 +0800

    post, follow and comment added.

--- src/main/java/com/ssglobal/revalida/jibe/config/UsersConfig.java
+++ src/main/java/com/ssglobal/revalida/jibe/config/UsersConfig.java
@@ -11,11 +11,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.ssglobal.revalida.jibe.model.Comment;
 import com.ssglobal.revalida.jibe.model.Follow;
+import com.ssglobal.revalida.jibe.model.Likes;
 import com.ssglobal.revalida.jibe.model.Post;
 import com.ssglobal.revalida.jibe.model.Role;
 import com.ssglobal.revalida.jibe.model.User;
 import com.ssglobal.revalida.jibe.repository.CommentRepository;
 import com.ssglobal.revalida.jibe.repository.FollowRepository;
+import com.ssglobal.revalida.jibe.repository.LikesRepository;
 import com.ssglobal.revalida.jibe.repository.PostRepository;
 import com.ssglobal.revalida.jibe.repository.UserRepository;
 
@@ -27,7 +29,7 @@ public class UsersConfig {
     private final PasswordEncoder passwordEncoder;
     @Bean
     CommandLineRunner commandLineRunnerUsers(UserRepository userRepository, PostRepository postRepository,
-    		CommentRepository commentRepository, FollowRepository followRepository) {
+    		CommentRepository commentRepository, FollowRepository followRepository, LikesRepository likesRepository) {
 
         return args -> {
             User user1 = User.builder()
@@ -174,10 +176,34 @@ public class UsersConfig {
             Post p7 = Post.builder().body("Post Body 7").datePosted(LocalDate.now().minusDays(5)).imageUrl("picsum.photos/200").user(userRepository.findById(10004).get()).build();
 
             Post p8 = Post.builder().body("ahh, geh, luh. \r\n"
-            		+ "yan yung pambansang ibon pre.").datePosted(LocalDate.now().minusDays(9)).imageUrl("https://media.tenor.com/SM55NxnE6_kAAAAM/lick-boy-lick.gif").user(userRepository.findById(10005).get()).build();
+            		+ "yan yung pambansang ibon pre.")
+            		.datePosted(LocalDate.now().minusDays(9))
+            		.imageUrl("https://media.tenor.com/SM55NxnE6_kAAAAM/lick-boy-lick.gif")
+            		.user(userRepository.findById(10005).get()).build();
+
+            Post p9= Post.builder().body("“Oo nga pala, hindi nga pala tayo”. "
+            		+ "It’s the first line as the guitar lick settles in that sends an arrow straight to the heart– an honest, straightforward reminder of what you aren’t.")
+            		.datePosted(LocalDate.now().minusDays(7))
+            		.imageUrl("https://i.ytimg.com/vi/mV8_c6XKG34/maxresdefault.jpg")
+            		.user(userRepository.findById(10006).get()).build();
+
+            Post p10= Post.builder().body("LUH! Kulang ka sa lambing gago")
+            		.datePosted(LocalDate.now().minusDays(2))
+            		.imageUrl("bit.ly/3ZtJM4v")
+            		.user(userRepository.findById(10007).get()).build();
+
+            Post p11= Post.builder().body("Selfie sa sea. kAs1 Ang F3eL1n6xz Q 4 u n3vEr m0nG mA se S3e")
+            		.datePosted(LocalDate.now().minusDays(5))
+            		.imageUrl("bit.ly/3mdRhhC")
+            		.user(userRepository.findById(10008).get()).build();
+
+            Post p12= Post.builder().body("boom")
+            		.datePosted(LocalDate.now().minusDays(1))
+            		.imageUrl("https://i.pinimg.com/236x/d7/00/fa/d700fa0fbfd40a47d94ea1435c45d7d5.jpg")
+            		.user(userRepository.findById(10009).get()).build();
 
             
-            List<Post> posts = List.of(p1, p2, p3, p4, p5, p6, p7, p8);
+            List<Post> posts = List.of(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12);
 
             postRepository.saveAll(posts);
             
@@ -248,8 +274,7 @@ public class UsersConfig {
             List<Comment> comment = List.of(c1, c2, c3, c4, c5, c6, c7, c8, c9);
             commentRepository.saveAll(comment);
 
-       
-        
+             
         Follow f1 = Follow.builder()
         		.followee(userRepository.findById(10005).get())
         		.follower(userRepository.findById(10006).get()).build();
@@ -335,6 +360,7 @@ public class UsersConfig {
         List<Follow> follow = List.of(f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12,
         		f13, f14, f15, f16, f17, f18, f19, f20);
         followRepository.saveAll(follow);
+      
         
         };
     }
